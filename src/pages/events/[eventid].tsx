213b38dafa0312daaab2a7d646a778cb83246f4e
Ilian Rusev <ilianrusev@gmail.com>
2023-01-19 13:29:51 +0200

    event detail components

--- src/pages/events/[eventid].tsx
+++ src/pages/events/[eventid].tsx
@@ -1,4 +1,16 @@
+import { getEventById } from "dummy-data";
+import { useRouter } from "next/router";
+
 function EventDetailsPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>no event found</p>;
+  }
+
   return (
     <div>
       <h1>event details</h1>
