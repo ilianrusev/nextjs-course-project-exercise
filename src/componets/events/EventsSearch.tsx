5f442eeaafdf5b3cb9468a31dc50ceb68bbcfdf0
Ilian Rusev <ilianrusev@gmail.com>
2023-01-19 14:05:51 +0200

    add filter logic for routing

--- src/componets/events/EventsSearch.tsx
+++ src/componets/events/EventsSearch.tsx
@@ -1,21 +1,34 @@
+import { useRef } from "react";
 import Button from "../ui/Button";
 
 import styles from "./EventsSearch.module.css";
 
 function EventsSearch(props: any) {
+  const yearRef = useRef();
+  const monthRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearRef.current.value;
+    const selectedMonth = monthRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.controls}>
         <div className={styles.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={styles.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,13 +1,21 @@
 import EventList from "@/componets/events/EventList";
 import EventsSearch from "@/componets/events/EventsSearch";
 import { getAllEvents } from "dummy-data";
+import { useRouter } from "next/router";
 
 function EventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year: string, month: string) {
+    const fullPath: string = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
     </>
   );
