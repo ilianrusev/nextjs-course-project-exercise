a48218fe9a55dd78dab1d573509d40f63f1e3dfd
Ilian Rusev <ilianrusev@gmail.com>
2023-01-23 20:24:18 +0200

    add input components

---
+++ src/componets/input/Comments.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import CommentList from "./CommentList";
+import NewComment from "./NewComment";
+import styles from "./Comments.module.css";
+
+function Comments(props: any) {
+  const { eventId } = props;
+
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  function addCommentHandler(commentData: any) {
+    // send data to API
+  }
+
+  return (
+    <section className={styles.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList />}
+    </section>
+  );
+}
+
+export default Comments;
