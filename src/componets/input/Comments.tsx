4c5e387843c8bd085654d2a0569bce191d3761dc
Ilian Rusev <ilianrusev@gmail.com>
2023-01-23 22:31:56 +0200

    notification in comments section

--- src/componets/input/Comments.tsx
+++ src/componets/input/Comments.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CommentList from "./CommentList";
 import NewComment from "./NewComment";
 import styles from "./Comments.module.css";
+import NotificationContext from "@/store/NotificationContext";
 
 function Comments(props: any) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState();
+
+  const notifCtx = useContext(NotificationContext);
 
   useEffect(() => {
     if (showComments) {
@@ -25,13 +28,39 @@ function Comments(props: any) {
   }
 
   function addCommentHandler(commentData: any) {
+    notifCtx.showNotification({
+      title: "Sending comment...",
+      message: "Your comment is currently being sent.",
+      status: "pending",
+    });
+
     fetch("/api/comments/" + eventId, {
       method: "POST",
       body: JSON.stringify(commentData),
       headers: { "Content-Type": "application/json" },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong");
+        });
+      })
+      .then((data) => {
+        notifCtx.showNotification({
+          title: "Success",
+          message: "Your comment was added",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notifCtx.showNotification({
+          title: "Error",
+          message: error.message || "something went wrong",
+          status: "error",
+        });
+      });
   }
 
   return (
