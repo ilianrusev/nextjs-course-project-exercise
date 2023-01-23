05911d18edb5f32a76096dd97fc3149c1f4fe680
Ilian Rusev <ilianrusev@gmail.com>
2023-01-23 22:13:56 +0200

    trigger and show notification

--- src/componets/input/NewsletterRegistration.tsx
+++ src/componets/input/NewsletterRegistration.tsx
@@ -1,21 +1,49 @@
-import { useRef } from "react";
+import NotificationContext from "@/store/NotificationContext";
+import { useContext, useRef } from "react";
 import styles from "./NewsletterRegistration.module.css";
 
 function NewsletterRegistration() {
   const emailInputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const notifCtx = useContext(NotificationContext);
 
   function registrationHandler(event: any) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
 
+    notifCtx.showNotification({
+      title: "Signing up...",
+      messge: "Registering for newsletter",
+      status: "pending",
+    });
+
     fetch("/api/newsletter", {
       method: "POST",
       body: JSON.stringify({ email: enteredEmail }),
       headers: { "Content-Type": "application/json" },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong");
+        });
+      })
+      .then((data) => {
+        notifCtx.showNotification({
+          title: "Success",
+          messge: "Successfully registered",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notifCtx.showNotification({
+          title: "Error",
+          messge: error.message || "something went wrong",
+          status: "error",
+        });
+      });
   }
 
   return (
